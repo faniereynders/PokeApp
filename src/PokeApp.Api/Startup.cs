aa8d7d782554a3daeba9637823a94053101ca4d3
Fanie Reynders <fanie@reynders.co>
2016-09-17 05:23:55 +0200

    initial commit

---
+++ src/PokeApp.Api/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace PokeApp.Api
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                const string logo = @"
+  ____       _     __    _                     _    ____ ___ 
+ |  _ \ ___ | | __/_/   / \   _ __  _ __      / \  |  _ \_ _|
+ | |_) / _ \| |/ / _ \ / _ \ | '_ \| '_ \    / _ \ | |_) | | 
+ |  __/ (_) |   <  __// ___ \| |_) | |_) |  / ___ \|  __/| | 
+ |_|   \___/|_|\_\___/_/   \_\ .__/| .__/  /_/   \_\_|  |___|
+                             |_|   |_|                       ";
+
+                return context.Response.WriteAsync(logo);
+            });
+        }
+    }
+}
