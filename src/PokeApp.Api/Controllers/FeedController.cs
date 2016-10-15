0f168c2ddfe011e80c98f77c8c31d295487de0d2
Fanie Reynders <fanie@reynders.co>
2016-10-15 16:19:05 +0200

    removed unused namespaces

--- src/PokeApp.Api/Controllers/FeedController.cs
+++ src/PokeApp.Api/Controllers/FeedController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PokeApp.Api.Data;
 using PokeApp.Api.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PokeApp.Api.Controllers
 {
